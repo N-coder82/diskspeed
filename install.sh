b0ac354c166af5038337ba818d2b1bbc93a0d96e
N-coder82 <redacted/private>
2024-06-15 15:32:18 -0700

    added vars to installer

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-
+progname="diskspeed"
 error_exit() {
     local RED='\033[0;31m'
     local NC='\033[0m' # No Color
     echo -e "${RED}An error occured:${NC} Please try again."
-    rm -rf "$TMPDIR/diskspeed"
+    rm -rf "$TMPDIR/$progname"
     exit 1
 }
 cd "$TMPDIR" || error_exit
-echo "Downloading diskspeed..."
-git clone https://github.com/N-coder82/diskspeed.git diskspeed
-cd "$TMPDIR/diskspeed" || error_exit
+echo "Downloading $progname..."
+git clone https://github.com/N-coder82/$progname.git $progname
+cd "$TMPDIR/$progname" || error_exit
 echo "Installing... (This may ask for sudo)"
-chmod +x diskspeed.sh
-sudo cp diskspeed.sh /usr/local/bin/diskspeed || error_exit
+chmod +x $progname.sh
+sudo cp $progname.sh /usr/local/bin/$progname || error_exit
 cd ..
-rm -rf "$TMPDIR/diskspeed" || error_exit
+rm -rf "$TMPDIR/$progname" || error_exit
 echo "Finished."
